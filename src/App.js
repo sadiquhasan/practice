5542c7b8d70559fd30434253bf31b8f6dcc9e7ce
mohammad sadiqu <mdsadiqu@quaratech.com>
2023-05-22 10:29:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+import Profile from "./component/Profile";
+import Product from "./component/Product";
+import Setting from "./component/Setting";
+import Home from "./component/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/setting" element={<Setting />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
   );
 }
 
